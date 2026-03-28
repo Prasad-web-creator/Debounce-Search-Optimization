429f2cc866bbc9dd55ee41d8babca282569926a3
Prasad-web-creator <magendraprasad84@gmail.com>
2026-03-28 10:33:24 +0530

    debounce

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+const search1 = document.getElementById('search-1')
+const search2 = document.getElementById('search-2')
+const result1 = document.getElementById('result-1')
+const result2 = document.getElementById('result-2')
+
+
+search1.addEventListener('keyup',(e)=>{
+    let value = e.target.value
+    if(String(value).length>49){
+        result1.textContent = "⚠️ Max 50 characters allowed"
+        search1.value = ""
+        return;
+    }
+    result1.textContent = "Search Results : " + value
+})
+
+const debouncedSearch = debounce(searchQuery,300)
+
+search2.addEventListener('keyup',(e)=>{
+    let value = e.target.value
+    if(String(value).length>49){
+        result2.textContent = "⚠️ Max 50 characters allowed"
+        search2.value = ""
+        return;
+    }
+    debouncedSearch(value)
+})
+
+function debounce(func,delay){
+    let timer;
+    return function(...args){
+        clearTimeout(timer)
+        timer = setTimeout(()=>{
+            func.apply(this,args)
+        },delay)
+    }
+}
+
+function searchQuery(value){
+    result2.textContent = "Search Results : " + value
+}
